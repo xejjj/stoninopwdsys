ba1afc9f9fc5c9fd8b4aca1ac6d05a9c0b1d0c5f
T1benn <stevenparras88@gmail.com>
2026-05-11 01:09:26 +0800

    database upd others disability

--- assets/stoninopwdsysdb.sql
+++ assets/stoninopwdsysdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2026 at 06:05 PM
+-- Generation Time: May 10, 2026 at 07:08 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -63,7 +63,7 @@ CREATE TABLE `archive` (
   `emergency_cont_num` varchar(255) NOT NULL,
   `emergency_cont_rel` varchar(255) NOT NULL,
   `socials` varchar(255) NOT NULL,
-  `disablity_type` enum('Cognitive','Visual','Physical','Auditory','Speech','Psychosocial') NOT NULL,
+  `disablity_type` enum('Cognitive','Visual','Physical','Auditory','Speech','Psychosocial','Others') NOT NULL,
   `disability_remarks` varchar(255) DEFAULT NULL,
   `resident_type` enum('PWD','CWD') NOT NULL,
   `guardian_name` varchar(255) NOT NULL,
@@ -102,7 +102,7 @@ CREATE TABLE `residents` (
   `emergency_cont_num` varchar(255) NOT NULL,
   `emergency_cont_rel` varchar(255) NOT NULL,
   `socials` varchar(255) NOT NULL,
-  `disablity_type` enum('Cognitive','Visual','Physical','Auditory','Speech','Psychosocial') NOT NULL,
+  `disablity_type` enum('Cognitive','Visual','Physical','Auditory','Speech','Psychosocial','Others') NOT NULL,
   `disability_remarks` varchar(255) DEFAULT NULL,
   `resident_type` enum('PWD','CWD') NOT NULL,
   `guardian_name` varchar(255) NOT NULL,
@@ -124,14 +124,14 @@ CREATE TABLE `residents` (
 --
 
 INSERT INTO `residents` (`ID`, `first_name`, `middle_name`, `last_name`, `civil_status`, `birthdate`, `age`, `birthplace`, `sex`, `address`, `contact_num`, `emergency_cont`, `emergency_cont_num`, `emergency_cont_rel`, `socials`, `disablity_type`, `disability_remarks`, `resident_type`, `guardian_name`, `guardian_cont_num`, `guardian_rel`, `father_name`, `mother_name`, `spouse_name`, `pwdid_num`, `control_num`, `idissue_date`, `idexpiration_date`, `profile`, `status`) VALUES
-(1, 'PERLITA', 'R', 'ABELLERA', '', '0000-00-00', 0, '', 'female', '63-C SANTOL ST.', '9472622313', '', '', '', '', 'Physical', NULL, 'PWD', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', 'Active'),
+(1, 'PERLITA', 'R', 'ABELLERA', '', '0000-00-00', 0, '', 'female', '63-C SANTOL ST.', '09472622313', '', '', '', '', 'Physical', NULL, 'PWD', 'MAGCAWAS, JESSIELEN', '', '', '', '', '', '109-525', '', '0000-00-00', '0000-00-00', '', 'Active'),
 (2, 'BRENT NYLSEN', 'PABLICO', 'AGUILLERA', '', '0000-00-00', 15, '', 'male', '68 WOMEN\'S CLUB ST.', '09158828474', '', '', '', '', 'Cognitive', NULL, 'CWD', 'AGUILLERA, LYN ROSE P.', '', '', '', '', '', '899-217', '', '0000-00-00', '0000-00-00', '', 'Active'),
-(3, 'ARLENE', 'ALFILER', 'AGUINALDO', '', '0000-00-00', 53, '', 'female', '65 SANTOL ST.', '09215751865', '', '', '', '', '', NULL, 'PWD', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', 'Active'),
-(4, 'ALLAN', 'HILANTAGAAN', 'ALABANZA', '', '0000-00-00', 50, '', 'male', '18 BIAK N BATO ST.', '09457821893', '', '', '', '', 'Visual', NULL, 'PWD', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', 'Active'),
-(5, 'NORMA', 'B', 'ALCUINO', '', '0000-00-00', 0, '', '', '88 WOMEN\'S CLUB ST.', '09291019040', '', '', '', '', '', NULL, 'PWD', 'SANTIAGO JR., CARLOS', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', 'Active'),
-(6, 'IISHA KARMEL', 'ULIT', 'ALDAY', '', '0000-00-00', 19, '', 'female', '15 BIAK NA BATO ST.', '09273242992', '', '', '', '', 'Auditory', NULL, 'PWD', 'ALDAY, ALLANDALE M.', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', 'Active'),
-(7, 'EDGILINE ', 'DENOFRA', 'AQUINO', '', '0000-00-00', 33, '', 'female', '93 WOMEN\'S CLUB ST.', '09171749249', '', '', '', '', 'Physical', NULL, 'PWD', 'AQUINO, GILDA', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', 'Active'),
-(8, 'LIZA', 'ARAGON', 'BACSAL', '', '0000-00-00', 32, '', 'female', '106 UNION CIVICA ST.', '09158935460', '', '', '', '', 'Speech', NULL, 'PWD', 'HOMBREBUENO, DOLORES', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', 'Active'),
+(3, 'ARLENE', 'ALFILER', 'AGUINALDO', '', '0000-00-00', 53, '', 'female', '65 SANTOL ST.', '09215751865', '', '', '', '', '', NULL, 'PWD', '', '', '', '', '', '', '12400000204300', '', '0000-00-00', '0000-00-00', '', 'Active'),
+(4, 'ALLAN', 'HILANTAGAAN', 'ALABANZA', '', '0000-00-00', 50, '', 'male', '18 BIAK N BATO ST.', '09457821893', '', '', '', '', 'Visual', NULL, 'PWD', '', '', '', '', '', '', '12400000974777', '', '0000-00-00', '0000-00-00', '', 'Active'),
+(5, 'NORMA', 'B', 'ALCUINO', '', '0000-00-00', 0, '', '', '88 WOMEN\'S CLUB ST.', '09291019040', '', '', '', '', '', NULL, 'PWD', 'SANTIAGO JR., CARLOS', '', '', '', '', '', '109-484', '', '0000-00-00', '0000-00-00', '', 'Active'),
+(6, 'IISHA KARMEL', 'ULIT', 'ALDAY', '', '0000-00-00', 19, '', 'female', '15 BIAK NA BATO ST.', '09273242992', '', '', '', '', 'Auditory', NULL, 'PWD', 'ALDAY, ALLANDALE M.', '', '', '', '', '', '21400000652846', '', '0000-00-00', '0000-00-00', '', 'Active'),
+(7, 'EDGILINE ', 'DENOFRA', 'AQUINO', '', '0000-00-00', 33, '', 'female', '93 WOMEN\'S CLUB ST.', '09171749249', '', '', '', '', 'Physical', NULL, 'PWD', 'AQUINO, GILDA', '', '', '', '', '', '1240001464691', '', '0000-00-00', '0000-00-00', '', 'Active'),
+(8, 'LIZA', 'ARAGON', 'BACSAL', '', '0000-00-00', 32, '', 'female', '106 UNION CIVICA ST.', '09158935460', '', '', '', '', 'Speech', NULL, 'PWD', 'HOMBREBUENO, DOLORES', '', '', '', '', '', '12400007487793', '', '0000-00-00', '0000-00-00', '', 'Active'),
 (9, 'LAN\'S', 'TAMAYO', 'BARTOLATA', '', '0000-00-00', 26, '', 'male', '6-8 BAGONG BUHAY ST.', '09555432325', '', '', '', '', 'Psychosocial', NULL, 'PWD', 'BARTOLATA, MARYANNE', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', 'Active'),
 (10, 'HARVEIAME', 'JUMAWAN', 'BARRACA', '', '0000-00-00', 2, '', 'male', '75B CUATRO DE JULIO ST.', '09274220847', '', '', '', '', 'Speech', NULL, 'CWD', 'JUMAWAN, SALVE REGINA', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', 'Active'),
 (11, 'NYMPHA', 'PANIZARES', 'BASCONES', '', '0000-00-00', 0, '', 'female', '43 TOMAS PINPIN ST.', '', '', '', '', '', 'Physical', NULL, '', 'BASCONES JR., GUILLERMO', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', 'Active'),
@@ -155,7 +155,17 @@ INSERT INTO `residents` (`ID`, `first_name`, `middle_name`, `last_name`, `civil_
 (32, 'ROMMEL', 'LLAMOSO', 'CRISTAL', '', '0000-00-00', 41, '', 'male', '114 BUSTAMANTE ST.', '09755481852', '', '', '', '', 'Physical', NULL, 'PWD', 'LUNA, REBECCA L.', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', 'Active'),
 (33, 'DARWIN', 'V', 'CRUZ', '', '0000-00-00', 31, '', 'male', '153 BATANES ST.', '', '', '', '', '', '', NULL, 'PWD', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', 'Active'),
 (34, 'JOHN RANDOL', 'E', 'CRUZ', '', '0000-00-00', 10, '', 'male', '81 CUATRO DE JULIO ST.', '09357421813', '', '', '', '', '', NULL, 'CWD', '', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', 'Active'),
-(35, 'KURT REY', 'REYES', 'CRUZ', '', '0000-00-00', 22, '', 'male', '7 MADIAS-AS ST.', '09501370574', '', '', '', '', 'Auditory', NULL, 'PWD', 'CRUZ, RAQUEL R.', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', 'Active');
+(35, 'KURT REY', 'REYES', 'CRUZ', '', '0000-00-00', 22, '', 'male', '7 MADIAS-AS ST.', '09501370574', '', '', '', '', 'Auditory', NULL, 'PWD', 'CRUZ, RAQUEL R.', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', 'Active'),
+(36, 'ALLAN', 'P', 'DAMES', '', '0000-00-00', 0, '', 'male', '112 LIBERATION ST.', '09950270700', '', '', '', '', '', NULL, '', 'DAMES, MAGDALENA', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', 'Active'),
+(37, 'CHRISTIE', 'RAMOS', 'DANGANAN', '', '0000-00-00', 51, '', 'female', '71B SAN ISIDRO ST.', '', '', '', '', '', 'Physical', NULL, 'PWD', 'DANGANAN, JOSELITO', '', '', '', '', '', '', '', '0000-00-00', '0000-00-00', '', 'Active'),
+(38, 'JOSE', '', 'DAYO JR.', '', '0000-00-00', 0, '', 'female', '82 BAGONG BUHAY ST', '', '', '', '', '', '', NULL, '', 'DAYO, BABY', '', '', '', '', '', '109-466', '', '0000-00-00', '0000-00-00', '', 'Active'),
+(39, 'RAUL', 'G', 'DE CASTRO JR.', '', '0000-00-00', 0, '', 'male', '115 BAGONG BUHAY ST.', '09061683132', '', '', '', '', '', NULL, '', 'DE CASTRO, RAQUEL', '', '', '', '', '', '109-420', '', '0000-00-00', '0000-00-00', '', 'Active'),
+(40, 'ROGEL', 'G', 'DE CASTRO', '', '0000-00-00', 0, '', 'male', '114 BAGONG BUHAY ST.', '09475803830', '', '', '', '', '', NULL, '', '', '', '', '', '', '', '109-509', '', '0000-00-00', '0000-00-00', '', 'Active'),
+(41, 'FERNANDO', 'FRIAS', 'DE GUZMAN', '', '0000-00-00', 46, '', 'male', '20 VISAYAN LIBERATION', '', '', '', '', '', 'Physical', 'CANCER', 'PWD', '', '', '', '', '', '', '138570', '', '0000-00-00', '0000-00-00', '', 'Active'),
+(42, 'MANUEL', 'GUEVARRA', 'DE GUZMAN', '', '0000-00-00', 0, '', 'male', '82 BAGONG BUHAY ST', '09269903907', '', '', '', '', 'Physical', NULL, '', 'DE GUZMAN, TERESITA', '', '', '', '', '', '109-116', '', '0000-00-00', '0000-00-00', '', 'Active'),
+(43, 'CATALINA', 'DIZON', 'DE JESUS', '', '0000-00-00', 0, '', 'female', '66 CUATRO DE JULIO ST.', '09497455168', '', '', '', '', 'Psychosocial', NULL, '', 'DE JESUS, RODOLFO', '', '', '', '', '', '109-389', '', '0000-00-00', '0000-00-00', '', 'Active'),
+(44, 'TAGUMPAY', 'DIZON', 'DE JESUS', '', '0000-00-00', 0, '', 'female', '66 CUATRO DE JULIO ST.', '09497455168', '', '', '', '', 'Cognitive', NULL, '', 'DE JESUS, RODOLFO', '', '', '', '', '', '109-388', '', '0000-00-00', '0000-00-00', '', 'Active'),
+(45, 'ELISA', 'A', 'DE LEON', '', '0000-00-00', 48, '', 'female', '75 WOMEN\'S CLUB ST.', '', '', '', '', '', '', NULL, '', '', '', '', '', '', '', '109-647', '', '0000-00-00', '0000-00-00', '', 'Active');
 
 --
 -- Indexes for dumped tables
@@ -194,13 +204,13 @@ ALTER TABLE `admincreds`
 -- AUTO_INCREMENT for table `archive`
 --
 ALTER TABLE `archive`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `residents`
 --
 ALTER TABLE `residents`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
